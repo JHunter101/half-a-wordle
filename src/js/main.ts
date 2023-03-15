d4e3cd5b61ee6bf5dc7feae2759408e0fe80fd17
Jack <jackchen8888@gmail.com>
2023-03-15 12:05:04 +0100

    partial rewrite of framework and conversion to ts

---
+++ src/js/main.ts
@@ -0,0 +1,338 @@
+export type LetterStatusLettersToIndex = {
+  [key: string]: number;
+};
+
+export type LetterStatusLetters = {
+  [key: string]: 'found' | 'seen' | 'unk';
+};
+
+export type LetterStatusIndex = {
+  [key: number]: 'found' | 'seen' | 'unk';
+};
+
+type BoardLetter = {
+  letter: string;
+  color: string;
+};
+
+function wordToRandom(str: string): number {
+  Math.seed = function (s: any) {
+    function letterToNumber(str: string): number {
+      let result = 0;
+      for (let i = 0; i < str.length; i++) {
+        const charCode = str.charCodeAt(i);
+        if (charCode >= 97 && charCode <= 122) {
+          // check if lowercase letter
+          result += charCode - 96; // subtract 96 to get 1-26 and convert to string
+        } else {
+          result += charCode - 64;
+        }
+      }
+      return result;
+    }
+
+    if (typeof s === 'string') {
+      s = letterToNumber(s);
+      console.log(s);
+    }
+
+    return function () {
+      console.log(s);
+      s = Math.sin(s) * 10000;
+      return s - Math.floor(s);
+    };
+  };
+
+  const random1 = Math.seed(str);
+  const random2 = Math.seed(random1());
+  Math.random = Math.seed(random2());
+  return Math.random();
+}
+
+const WORD_DATA_SET = require('./wd/WORD_DATA_SET.js');
+// Function to submit a user's guess and update the game board and letter status
+function SubmitWord(
+  gameBoard: BoardLetter[][],
+  letterStatus: LetterStatusLetters,
+  guess: string,
+  goalWord: string,
+) {
+  // Function to determine if a guess is valid (i.e., uses only valid letters and is the correct length)
+  function IsWordValid(guess: string, goalWord: string) {
+    if (guess.length === goalWord.length) {
+      if (WORD_DATA_SET.has(guess)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // Function to calculate the fitness of a guess (i.e., how many letters are in the correct position and how many are in the word but in the wrong position)
+  function GetWordFitness(guess: string, goalWord: string) {
+    const guessLetters = guess.split('');
+    const goalLetters = goalWord.split('');
+    const fitness: LetterStatusIndex = {};
+
+    const guessLettersCounter: LetterStatusLettersToIndex = {};
+    const goalLettersCounter: LetterStatusLettersToIndex = {};
+
+    for (let i = 0; i < guessLetters.length; i++) {
+      guessLettersCounter[guessLetters[i]] = guessLettersCounter[
+        guessLetters[i]
+      ]
+        ? guessLettersCounter[guessLetters[i]] + 1
+        : 1;
+
+      goalLettersCounter[guessLetters[i]] = goalLettersCounter[guessLetters[i]]
+        ? goalLettersCounter[guessLetters[i]] + 1
+        : 1;
+
+      if (goalLetters.includes(guessLetters[i])) {
+        if (guessLetters[i] === goalLetters[i]) {
+          fitness[i] = 'found';
+        } else {
+          fitness[i] = 'seen';
+        }
+      } else {
+        fitness[i] = 'unk';
+      }
+    }
+
+    for (let i = guessLetters.length - 1; i >= 0; i--) {
+      if (goalLetters.includes(guessLetters[i])) {
+        if (!(fitness[i] === 'found')) {
+          if (
+            guessLettersCounter[guessLetters[i]] >
+            goalLettersCounter[guessLetters[i]]
+          ) {
+            fitness[i] = 'unk';
+            guessLettersCounter[guessLetters[i]] -= 1;
+          }
+        }
+      }
+    }
+    return fitness;
+  }
+
+  // Function to update the letter status based on the fitness of the guess
+  function UpdateLetterStatus(
+    letterStatus: LetterStatusLetters,
+    guess: string,
+    fitness: LetterStatusIndex,
+  ) {
+    for (const k in fitness) {
+      if (fitness[k] === 'found') {
+        letterStatus[Array.from(guess)[k]] = fitness[k];
+      } else {
+        if (fitness[k] === 'seen') {
+          letterStatus[Array.from(guess)[k]] = fitness[k];
+        } else if (fitness[k] === 'unk') {
+          letterStatus[Array.from(guess)[k]] = fitness[k];
+        }
+      }
+    }
+    return letterStatus;
+  }
+
+  // Function to update the game board based on the guess and its fitness
+  function UpdateGameBoard(
+    gameBoard: BoardLetter[][],
+    guess: string,
+    fitness: LetterStatusIndex,
+  ) {
+    let y = 0;
+    while (gameBoard[y][0].letter !== '-') {
+      y += 1;
+    }
+
+    const wordLetters = guess.split('');
+    for (let x = 0; x < wordLetters.length; x++) {
+      gameBoard[y][x].letter = wordLetters[x];
+      gameBoard[y][x].color = fitness[x];
+    }
+
+    return gameBoard;
+  }
+  guess = guess.toLowerCase(); // convert to lowercase to standardize input
+
+  if (!IsWordValid(guess, goalWord)) {
+    console.log('Invalid guess');
+    return { gameBoard, letterStatus }; // TO DO RETRY
+  }
+
+  const fitness = GetWordFitness(guess, goalWord);
+  gameBoard = UpdateGameBoard(gameBoard, guess, fitness);
+  letterStatus = UpdateLetterStatus(letterStatus, guess, fitness);
+  return { gameBoard, letterStatus };
+}
+
+function InitGame(
+  lCount: number,
+  rows: number,
+  preFilled: number,
+  difficulty: number,
+) {
+  // Function to initialize the status of each letter
+  function InitLetterStatus(): LetterStatusIndex {
+    const letterStatus = {
+      a: 'unk',
+      b: 'unk',
+      c: 'unk',
+      d: 'unk',
+      e: 'unk',
+      f: 'unk',
+      g: 'unk',
+      h: 'unk',
+      i: 'unk',
+      j: 'unk',
+      k: 'unk',
+      l: 'unk',
+      m: 'unk',
+      n: 'unk',
+      o: 'unk',
+      p: 'unk',
+      q: 'unk',
+      r: 'unk',
+      s: 'unk',
+      t: 'unk',
+      u: 'unk',
+      v: 'unk',
+      w: 'unk',
+      x: 'unk',
+      y: 'unk',
+      z: 'unk',
+    };
+    return letterStatus;
+  }
+
+  function InitBoard(
+    letterStatus: LetterStatusLetters,
+    goalWord: string,
+    lCount: number,
+    rows = 6,
+    preFilled = 2,
+    difficulty: number,
+  ): {
+    gameBoard: BoardLetter[][];
+    letterStatus: LetterStatusLetters;
+    goalWord: string;
+    lCount: number;
+    rows: number;
+    preFilled: number;
+    difficulty: number;
+  } {
+    const gameBoard = [];
+    for (let y = 0; y < rows; y++) {
+      const row = [];
+      for (let x = 0; x < lCount; x++) {
+        row[x] = {
+          letter: '-',
+          color: 'unk',
+        };
+      }
+      gameBoard[y] = row;
+    }
+    return PreFillBoard(
+      gameBoard,
+      letterStatus,
+      goalWord,
+      rows,
+      preFilled,
+      difficulty,
+    );
+  }
+
+  function GenerateWord(
+    lCount: number,
+    difficulty: number,
+    goalWord = '',
+  ): string {
+    function GetWordData(lCount: number, difficulty = 15) {
+      const path = './wd/wd_' + String(difficulty) + 'K.js';
+      const module = require(path);
+      return module[lCount];
+    }
+
+    const words = GetWordData(lCount, difficulty);
+    if (goalWord === '') {
+      return words[Math.floor(Math.random() * words.length)];
+    } else {
+      // TODO SMART PICK
+      const i1 = Math.floor(wordToRandom(goalWord) * lCount);
+      const i2 = Math.floor(wordToRandom(goalWord + goalWord) * lCount);
+      const PARTIAL_WORD_DATA_SET = Array.from(WORD_DATA_SET).filter(
+        (word) =>
+          word.split('').length === goalWord.length &&
+          word.includes(goalWord[i1]) &&
+          word.includes(goalWord[i2]),
+      );
+      return PARTIAL_WORD_DATA_SET[
+        Math.floor(Math.random() * PARTIAL_WORD_DATA_SET.length)
+      ];
+    }
+  }
+
+  function PreFillBoard(
+    gameBoard: BoardLetter[][],
+    letterStatus: LetterStatusLetters,
+    goalWord: string,
+    rows = 6,
+    preFilled = 2,
+    difficulty = 15,
+  ): {
+    gameBoard: BoardLetter[][];
+    letterStatus: LetterStatusLetters;
+    goalWord: string;
+    lCount: number;
+    rows: number;
+    preFilled: number;
+    difficulty: number;
+  } {
+    if (preFilled === 0) {
+      return {
+        gameBoard,
+        letterStatus,
+        goalWord,
+        lCount: rows,
+        rows,
+        preFilled,
+        difficulty,
+      };
+    }
+    const guess = GenerateWord(lCount, difficulty, goalWord);
+    const newBoardAndStatus = SubmitWord(
+      gameBoard,
+      letterStatus,
+      guess,
+      goalWord,
+    );
+    return PreFillBoard(
+      newBoardAndStatus.gameBoard,
+      newBoardAndStatus.letterStatus,
+      goalWord,
+      rows,
+      preFilled - 1,
+      difficulty,
+    );
+  }
+
+  const goalWord = GenerateWord(lCount, difficulty);
+  console.log(goalWord);
+  const letterStatus = InitLetterStatus();
+  return InitBoard(letterStatus, goalWord, lCount, rows, preFilled, difficulty);
+}
+
+const lCount = 5;
+const difficulty = 15;
+const rows = 6;
+const preFilled = 2;
+const newBoardAndStatus = InitGame(lCount, rows, preFilled, difficulty);
+const gameBoard = newBoardAndStatus.gameBoard;
+const letterStatus = newBoardAndStatus.letterStatus;
+
+if (gameBoard[gameBoard.length - 1][0].letter !== '-') {
+  console.log('Done!');
+}
+
+console.log(gameBoard);
+console.log(letterStatus);
---
+++ tools/WD.ts
@@ -0,0 +1,159 @@
+// Import required libraries
+const fs = require('fs');
+const path = require('path');
+
+function parseCSV(str) {
+  const arr = [];
+  let quote = false; // 'true' means we're inside a quoted field
+
+  // Iterate over each character, keep track of current row and column (of the returned array)
+  for (let row = 0, col = 0, c = 0; c < str.length; c++) {
+    const cc = str[c];
+    const nc = str[c + 1]; // Current character, next character
+    arr[row] = arr[row] || []; // Create a new row if necessary
+    arr[row][col] = arr[row][col] || ''; // Create a new column (start with empty string) if necessary
+
+    // If the current character is a quotation mark, and we're inside a
+    // quoted field, and the next character is also a quotation mark,
+    // add a quotation mark to the current column and skip the next character
+    if (cc === '"' && quote && nc === '"') {
+      arr[row][col] += cc;
+      ++c;
+      continue;
+    }
+
+    // If it's just one quotation mark, begin/end quoted field
+    if (cc === '"') {
+      quote = !quote;
+      continue;
+    }
+
+    // If it's a comma and we're not in a quoted field, move on to the next column
+    if (cc === ',' && !quote) {
+      ++col;
+      continue;
+    }
+
+    // If it's a newline (CRLF) and we're not in a quoted field, skip the next character
+    // and move on to the next row and move to column 0 of that new row
+    if (cc === '\r' && nc === '\n' && !quote) {
+      ++row;
+      col = 0;
+      ++c;
+      continue;
+    }
+
+    // If it's a newline (LF or CR) and we're not in a quoted field,
+    // move on to the next row and move to column 0 of that new row
+    if (cc === '\n' && !quote) {
+      ++row;
+      col = 0;
+      continue;
+    }
+    if (cc === '\r' && !quote) {
+      ++row;
+      col = 0;
+      continue;
+    }
+
+    // Otherwise, append the current character to the current column
+    arr[row][col] += cc;
+  }
+  return arr;
+}
+
+// Define a function to save data to a file
+function saveToFile(data, fileName) {
+  data = JSON.stringify(data);
+  fs.writeFile(
+    path.join(__dirname, fileName + '.js'),
+    `/* eslint-disable semi */
+/* eslint-disable eol-last */
+/* eslint-disable object-curly-spacing */
+/* eslint-disable key-spacing */
+/* eslint-disable quote-props */
+/* eslint-disable comma-spacing */
+/* eslint-disable quotes */
+/* eslint-disable no-unused-vars */
+const ${fileName} = ${data}
+module.exports = ${fileName};
+`,
+    function (err) {
+      if (err) throw err;
+      console.log(`${fileName} saved!`);
+    },
+  );
+}
+
+// Define a function for linear normalization
+function linearNormalize(x, xMin = 0, xMax = 1, yMin = 1, yMax = 10) {
+  const xRange = xMax - xMin;
+  const yRange = yMax - yMin;
+  const y = ((x - xMin) / xRange) * yRange + yMin;
+  return Math.ceil(y);
+}
+
+// Read the word data from a CSV file
+const wordData = parseCSV(
+  fs.readFileSync(path.join(__dirname, 'word_data.csv'), 'utf8'),
+  {
+    headers: false,
+    skip_empty_lines: true,
+  },
+);
+
+// Filter out rows with missing values and short strings
+const filteredData = wordData.filter(
+  (row) => row[0] && row[0].length >= 5 && row[0].length <= 12,
+);
+
+// Initialize an object for storing word data by length
+const wordDataListJS = {};
+for (let i = 5; i <= 12; i++) {
+  wordDataListJS[i] = [];
+}
+
+// Initialize a set for storing unique words
+const wordDataSetJS = new Set();
+
+// Loop through each row in the filtered data
+let j = 0;
+const ticks = new Set([5000, 15000, 25000, 35000]);
+filteredData.forEach((row) => {
+  if (ticks.has(j)) {
+    console.log(j);
+    saveToFile(wordDataListJS, 'WD_' + Math.floor(j / 1000) + 'K');
+  }
+  j++;
+
+  const word = row[0];
+  const wordLength = word.length;
+
+  // Add the word to the unique word set
+  wordDataSetJS.add(word);
+  const normalizedF = linearNormalize(row[1], 0, 1226734006, 1, 10);
+  for (let i = 0; i < normalizedF; i++) {
+    wordDataListJS[wordLength].push(word);
+  }
+});
+
+const fileName = 'WORD_DATA_SET';
+const data = JSON.stringify(Array.from(wordDataSetJS));
+fs.writeFile(
+  path.join(__dirname, fileName + '.js'),
+  `/* eslint-disable semi */
+/* eslint-disable eol-last */
+/* eslint-disable object-curly-spacing */
+/* eslint-disable key-spacing */
+/* eslint-disable quote-props */
+/* eslint-disable comma-spacing */
+/* eslint-disable quotes */
+/* eslint-disable no-unused-vars */
+const ${fileName} = new Set(${data})
+module.exports = ${fileName};
+`,
+  function (err) {
+    if (err) throw err;
+    console.log(`${fileName} saved!`);
+  },
+);
